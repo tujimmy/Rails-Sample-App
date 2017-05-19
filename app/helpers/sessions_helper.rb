d76ee4c063794b0ed3e0a51407342e0d75552b08
JTu <jimmy.tu@live.ca>
2017-05-19 17:30:06 +0000

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,16 +6,39 @@ module SessionsHelper
   def current_user 
     # User object is true, the call to find_by only gets executed
     # if current_user hasn't yet been assigned
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id])
+    elsif (user_id = cookies.signed[:user_id])
+      # raise # The tests still pass, so this branch is currently untested.
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
   
   def logged_in?
     !current_user.nil?
   end
   
+  # forgets a persistent session 9.12
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+  
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
   
 end
